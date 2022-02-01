82a70e6c5c7ea8d6b50f24dc5a78952e646afeeb
Imam H <97616881+IHCodes@users.noreply.github.com>
2022-02-01 17:28:45 +0000

    IH 02/02/22

---
+++ BudgetApp.py
@@ -0,0 +1,9 @@
+class Budget:
+    def __init__(self):
+        self.balance = 0.0
+    
+    def deposit(self, amount):
+        self.balance =+ amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
---
+++ main.py
@@ -0,0 +1,10 @@
+from BudgetApp import Budget
+
+food = Budget()
+food.deposit(100)
+
+print(food.balance)
+
+food.withdraw(30)
+print(food.balance)
+
